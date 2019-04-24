9cb3e23d664c2ad09056f39363816a8632cb2703
Alejandro Lliso <alejandro.lliso.cosin@gmail.com>
2019-04-24 21:53:36 +0200

    Updates

---
+++ precache-manifest.12b0add7d1ce1982dfba0a974e8d8036.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7e2bec447d686b5496be74f8aaeeea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5765e1581a5415856a55",
+    "url": "/static/css/main.a706fe7c.chunk.css"
+  },
+  {
+    "revision": "f40a22ea239819a49dcc",
+    "url": "/static/js/2.aec0d048.chunk.js"
+  },
+  {
+    "revision": "5765e1581a5415856a55",
+    "url": "/static/js/main.c495f4e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.380adf610922866d450d1a7155a60b54.js"
+  "/precache-manifest.12b0add7d1ce1982dfba0a974e8d8036.js"
 );
 
 self.addEventListener('message', (event) => {
